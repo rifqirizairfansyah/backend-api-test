46dbdbea5aeb143bfa0c507a41163c2777833b47
rifqirizairfansyah <38659907+rifqirizairfansyah@users.noreply.github.com>
2022-12-28 00:35:50 +0700

    add feature failed attemps and backoff next 1 hour

--- src/queque/order-queue.js
+++ src/queque/order-queue.js
@@ -14,6 +14,14 @@ orderQueue.on('failed', handlerFailure);
 orderQueue.on('completed', handlerCompleted);
 orderQueue.on('stalled', handlerStalled);
 
+/**
+ * @function createNewOrder
+ * Create new order and send to Redis
+ * when making an order fails, it will be retried 3 times in the next 1 hour
+ * @param {String} user, timezone, date
+ * @returns {Promise<{code: number, message: string, status: boolean}>}
+ */
+
 const createNewOrder = (user, timezone, date) => {
   const offsetZone = getTimezoneOffset(timezone)
   const day = getDate(date)
@@ -25,6 +33,7 @@ const createNewOrder = (user, timezone, date) => {
     date
   }, {
     attempts: 3,
+    backoff: 3600000, 
     removeOnFail: false,
     repeat: { 
       cron: `00 09 ${day} ${month} *`,
